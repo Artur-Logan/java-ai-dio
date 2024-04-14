adc2bb06e28a03f1cbc4bf3c9a8cb80e1fad4893
Artur Logan <arturlogan1ferreira@gmail.com>
2024-04-14 13:42:10 -0300

    Primeiro Commit

---
+++ src/main/java/com/arturlogan/javaaidio/JavaAiDioApplication.java
@@ -0,0 +1,13 @@
+package com.arturlogan.javaaidio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAiDioApplication.class, args);
+	}
+
+}
